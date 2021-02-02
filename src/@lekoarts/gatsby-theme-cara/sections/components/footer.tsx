5618e93866b461c248631e81c69fc025a28f24cb
Matthew <matthew.davenport30@gmail.com>
2021-02-02 19:51:34 +1000

    Renamed footer and created new file

---
+++ src/@lekoarts/gatsby-theme-cara/sections/components/footer.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+}
+
+export default Footer
